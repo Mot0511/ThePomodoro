a5509322811ed71d5aa500bd3d93064c41f281d3
Mot0511 <suvorov.matvej9@gmail.com>
2023-10-22 12:15:48 +0300

    _

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
-const workTime = document.getElementById('work-time').value
-const restTime = document.getElementById('rest-time').value
-const bigRestTime = document.getElementById('big-rest-time').value
+const workTime = Number(document.getElementById('work-time').value)
+const restTime = Number(document.getElementById('rest-time').value)
+const bigRestTime = Number(document.getElementById('big-rest-time').value)
 
 const timerLabel = document.getElementById('timer-label')
 const timer = document.getElementById('timer')
 
 let run = false
 let minutes = 0
-let seconds = 57
+let seconds = 0
 
 let stage = 0
 let round = 1
 
 const running = () => {
-    setInterval(() => {
-        seconds++
-        if (seconds > 59){
-            seconds = 0
-            minutes++
-        }
-        console.log(run);
+    const intervalId = setInterval(() => {
         if (run){
+            seconds++
+            if (seconds > 59){
+                seconds = 0
+                minutes++
+            }
+            console.log(run);
             setLabel()
             checkStage()
         } else{
-
+            clearInterval(intervalId)
         }
 
     }, 1000)
 }
 
 const checkStage = () => {
-    if (stage === 1 && minutes === workTime){
-        console.log('Rest')
-        stage = 2
-        timerLabel.innerHTML = 'Отдых'
+    const audio = new Audio("./sound.mp3");
+    const audio2 = new Audio("./sound2.mp3");
+    console.log(round);
+    if (stage === 1 && minutes === workTime && round === 5){
+        console.log('Big rest')
+        stage = 3
+        timerLabel.innerHTML = 'Большой отдых'
         minutes = 0
-        seconds = 55
+        seconds = 0
+        audio2.play()
     } else if (stage === 2 && minutes === restTime){
         round++
         console.log('Work')
         stage = 1
         timerLabel.innerHTML = 'Работа'
         minutes = 0
-        seconds = 55
-    } else if (stage === 1 && minutes === workTime && round === 5){
-        console.log('Big rest')
-        stage = 3
-        timerLabel.innerHTML = 'Большой отдых'
+        seconds = 0
+        audio.play()
+    } else if (stage === 1 && minutes === workTime){
+        console.log('Rest')
+        stage = 2
+        timerLabel.innerHTML = 'Отдых'
         minutes = 0
-        seconds = 55
+        seconds = 0
+        audio.play()
     } else if (stage === 3 && minutes === bigRestTime) {
         console.log('Work')
         stage = 1
         timerLabel.innerHTML = 'Работа'
         minutes = 0
-        seconds = 55
+        seconds = 0
         round = 0
+        audio.play()
     }
+
     setLabel()
 }
 
